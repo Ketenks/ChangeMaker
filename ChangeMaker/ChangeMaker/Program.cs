f7b7225253db5ba836ab3d07ba97f25ab7efb3ba
Kevin Snow <krsnow77@zoho.com>
2014-06-23 16:24:07 -0600

    ChangeMaker
    changes out a money amount into the least amount of coins

---
+++ ChangeMaker/ChangeMaker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        //change maker: round money to penny and 
+        //compute minimum amount of coins to make change
+        static void ChangeMaker(double amount)
+        {
+
+        }
+    }
+}
