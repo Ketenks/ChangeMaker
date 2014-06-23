8c5b97a0548eac3416e1122659e48fe19d37b2bb
Kevin Snow <krsnow77@zoho.com>
2014-06-23 17:03:36 -0600

    ChangeMaker
    print minimum number of coins for an amount of money

--- ChangeMaker/ChangeMaker/Program.cs
+++ ChangeMaker/ChangeMaker/Program.cs
@@ -10,12 +10,63 @@ namespace ChangeMaker
     {
         static void Main(string[] args)
         {
+            //CALLING FUNCTIONS HERE
+            Console.WriteLine("CHANGE MAKER!!!!!");
+            Console.WriteLine();
+            ChangeMaker(3.18);
+            Console.WriteLine();
+            ChangeMaker(0.99);
+            Console.WriteLine();
+            ChangeMaker(12.93);
+
+            //keeps console open
+            Console.ReadKey();
         }
-        //change maker: round money to penny and 
+        //change maker: round money amount to penny and 
         //compute minimum amount of coins to make change
         static void ChangeMaker(double amount)
         {
+            //declare function variables
+            int quarter = 25;
+            int numQuarter = 0;
+            int dime = 10;
+            int numDime = 0;
+            int nickel = 5;
+            int numNickel = 0;
+            int penny = 1;
+            int numPenny = 0;
 
+            //code to perform the changes to amount
+            int amountC = Convert.ToInt32(amount * 100);
+           
+           while (amountC >= quarter)
+           {
+               amountC -= quarter;
+               numQuarter++;
+           }
+           while (amountC >= dime)
+           {
+               amountC -= dime;
+               numDime++;
+           }
+           while (amountC >= nickel)
+           {
+               amountC -= nickel;
+               numNickel++;
+           }
+           while (amountC >= penny)
+           {
+               amountC -= penny;
+               numPenny++;
+           }
+
+
+            //write ouput to the console
+            Console.WriteLine("Amount: " + "$" + amount);
+            Console.WriteLine("Quarters: " + numQuarter);
+            Console.WriteLine("Dimes: " + numDime);
+            Console.WriteLine("Nickels: " + numNickel);
+            Console.WriteLine("Pennies: " + numPenny);
         }
     }
 }
