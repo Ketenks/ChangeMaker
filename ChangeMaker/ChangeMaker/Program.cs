581d4fa6d3ebb53f254b41646d4620bf3426cc07
Kevin Snow <krsnow77@zoho.com>
2014-06-24 08:25:10 -0600

    Change to the changeMaker
    i put in the code for the rest of the dollar bills as well

--- ChangeMaker/ChangeMaker/Program.cs
+++ ChangeMaker/ChangeMaker/Program.cs
@@ -18,6 +18,11 @@ namespace ChangeMaker
             ChangeMaker(0.99);
             Console.WriteLine();
             ChangeMaker(12.93);
+            Console.WriteLine();
+            Console.WriteLine("Let's bring out the BIG BUCKS!");
+            ChangeMaker(11234.38);
+            Console.WriteLine();
+            ChangeMaker(4875874.72);
 
             //keeps console open
             Console.ReadKey();
@@ -27,6 +32,18 @@ namespace ChangeMaker
         static void ChangeMaker(double amount)
         {
             //declare function variables
+            int oneHundDollar = 10000;
+            int numOneHund = 0;
+            int fiftyDollar = 5000;
+            int numFifty = 0;
+            int twentyDollar = 2000;
+            int numTwenty = 0;
+            int tenDollar = 1000;
+            int numTen = 0;
+            int fiveDollar = 500;
+            int numFive = 0;
+            int oneDollar = 100;
+            int numOne = 0;
             int quarter = 25;
             int numQuarter = 0;
             int dime = 10;
@@ -38,7 +55,36 @@ namespace ChangeMaker
 
             //code to perform the changes to amount
             int amountC = Convert.ToInt32(amount * 100);
-           
+            while (amountC >= oneHundDollar)
+            {
+                amountC -= oneHundDollar;
+                numOneHund++;
+            }
+            while (amountC >= fiftyDollar)
+            {
+                amountC -= fiftyDollar;
+                numFifty++;
+            }
+            while (amountC >= twentyDollar)
+            {
+                amountC -= twentyDollar;
+                numTwenty++;
+            }
+            while (amountC >= tenDollar)
+            {
+                amountC -= tenDollar;
+                numTen++;
+            }
+            while (amountC >= fiveDollar)
+            {
+                amountC -= fiveDollar;
+                numFive++;
+            }
+            while (amountC >= oneDollar)
+            {
+                amountC -= oneDollar;
+                numOne++;
+            }
            while (amountC >= quarter)
            {
                amountC -= quarter;
@@ -63,6 +109,12 @@ namespace ChangeMaker
 
             //write ouput to the console
             Console.WriteLine("Amount: " + "$" + amount);
+            Console.WriteLine("Hundreds: " + numOneHund);
+            Console.WriteLine("Fifties: " + numFifty);
+            Console.WriteLine("Twenties: " + numTwenty);
+            Console.WriteLine("Tens: " + numTen);
+            Console.WriteLine("Fives: " + numFive);
+            Console.WriteLine("Ones: " + numOne);
             Console.WriteLine("Quarters: " + numQuarter);
             Console.WriteLine("Dimes: " + numDime);
             Console.WriteLine("Nickels: " + numNickel);
